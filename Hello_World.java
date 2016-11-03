f591c87bdcffe43f1223baf6764fd9d991177df2
Huhas <lukasmykael@me.com>
2016-11-02 20:16:45 -0500

    Add files via upload
    Test!

---
+++ Hello_World.java
@@ -0,0 +1,16 @@
+
+
+public class Hello_World {
+	
+	public static void main(String[] args) {
+		//print 
+	
+		
+			System.out.println("hello, world");
+			System.out.println("Hello, Lukas.");
+			
+		
+		
+		}
+		
+}
